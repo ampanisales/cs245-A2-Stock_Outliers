a48e77a83288d0d08b24e91b1c9bf7c5d4d0cd69
ampanisales <31353791+ampanisales@users.noreply.github.com>
2017-10-13 15:53:09 -0700

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,129 +1,122 @@
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-public class LinkedList {
+public class StockOutliers {
 	
-	Link head;
-	int size = 0;
-	
-	//Is doing this ok?
-	public class ListIterator {
-
-		private Link link;
+	public static void main(String args[]) {
 		
-		public ListIterator() {
-			link = head;
-		}
+		Scanner scan = new Scanner(System.in);
 		
-		public boolean hasNext() {
-			return link.getNext() != null;
-		}
-		
-		public Object next() {
+		//DO INPUT VALIDATION
+		boolean fileExists = false;
+		while (!fileExists) {
 			try {
-				link = link.getNext();
-				return link.getData();
+				System.out.print("Stock Symbol: ");
+				String symbol = scan.next();
+				String filename = symbol + ".csv";
+				Scanner filescan = new Scanner(new File(filename));
+				//Check if startDate is in right format? Check if startDate is a valid date?
+				System.out.print("Starting Date (Use year-month-day format): ");
+				String date = scan.next();
+				String format = "yyyy-MM-dd";
+				Date startDate = new SimpleDateFormat(format).parse(date);
+				Calendar endCal = Calendar.getInstance();
+				endCal.setTime(startDate);
+				endCal.add(Calendar.YEAR, 1);
+				Date endDate = endCal.getTime();
+				LinkedList allRecords = new LinkedList();
+				//ArrayList<StockData> allRecords = new ArrayList<StockData>(); //TEST
+				
+				filescan.nextLine(); //Makes sure first line isn't used
+				filescan.useDelimiter(Pattern.compile(",|;"));
+				
+				int recordsInRange = 0;
+				double sumAdjClose = 0;
+				double sumXY = 0;
+				int sumXsquared = 0;
+				
+				while (filescan.hasNextLine()) {
+					StockData data = new StockData();
+					String tempDate = filescan.next().replace("\n", "").replace("\r", "");
+					if (tempDate.equals("")) { //For last line in file
+						break;
+					} else {
+						data.setDate(tempDate);
+					}
+					Date recordDate = new SimpleDateFormat(format).parse(data.getDate());
+					data.setOpen(filescan.next());
+					data.setHigh(filescan.next());
+					data.setLow(filescan.next());
+					data.setClose(filescan.next());
+					data.setAdjClose(filescan.next());
+					data.setVolume(filescan.next());
+					allRecords.add(data);
+					//If record is within the range
+					if ((recordDate.equals(startDate) || recordDate.after(startDate))
+							&& recordDate.before(endDate)) {
+						recordsInRange++; //This is n
+						data.setInRange(true);
+						data.setDayInRange(recordsInRange);
+						sumXY += recordsInRange*Double.parseDouble(data.getAdjClose());
+						sumXsquared += recordsInRange*recordsInRange;
+						sumAdjClose += Double.parseDouble(data.getAdjClose()); 
+					}
+				}
+				filescan.close();
+				
+				if (recordsInRange < 2) {
+					System.out.println("There is insufficient data for this period.");
+				} else {
+					//Make outliers linked List
+					LinkedList outliers = new LinkedList();
+					OutlierDetector detector = new OutlierDetector(recordsInRange, sumAdjClose,
+							recordsInRange, sumXY, sumXsquared);
+					LinkedList.ListIterator it = allRecords.iterator();
+					while (it.hasNext()) {
+						StockData record = (StockData) it.next();
+						Double adjClose = Double.parseDouble(record.getAdjClose()); 
+						if (record.getInRange()  && detector.isOutlier(record.getDayInRange(), adjClose)) {
+							outliers.add(record);
+						}
+					}
+					
+					//Prints linked list of outliers
+					System.out.println("Outliers from " + startDate + " to " + endDate);
+					it = outliers.iterator();
+					DecimalFormat closeFormat = new DecimalFormat(".##");
+					while (it.hasNext()) {
+						StockData outlier = (StockData) it.next();
+						Double adjClose = Double.parseDouble(outlier.getAdjClose()); 
+						System.out.println(outlier.getDate() + ": " + closeFormat.format(adjClose));
+					}
+					break;
+					
+		//				//TEST PRINT
+		//				for (int i = 0; i < allRecords.size(); i++) {
+		//					if (i % 3 == 0) {
+		//						System.out.println(allRecords.get(i).getDate() + ": " + allRecords.get(i).getAdjClose());
+		//					}
+		//				}
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println("File of this stock not found");
+				continue;
+			} catch (ParseException e) {
+				System.out.println("Invalid date");
+				continue;
 			} catch (Exception e) {
-				return null;
+				e.printStackTrace();
+				System.out.println(e);
+				continue;
 			}
+			scan.close();
 		}
-	}
-	
-	public LinkedList() {
-		//Dummy head for iterator
-		head = new Link(null);
-	}
-	
-	//Add to end of list
-	public void add(Object data) {
-		Link currLink = head;
-		if (currLink == null) {
-			head = new Link(data);
-		} else {
-			while (currLink.getNext() != null) {
-				currLink = currLink.getNext();
-			}
-			currLink.setNext(new Link(data));
-		}
-		size++;
-	}
-	
-	public void add(Object data, int pos) throws Exception {
-		/* 1. Error check: Is the position valid?
-		2. Create a Link instance. Populate with argument Object
-		3. Find (“curr”) pos-1
-		4. Adjust pointers
-			link.next = curr.next
-			curr.next = link	
-		5. Update size	*/
-		
-		if (pos < 0 || pos > size) {
-			throw new Exception();
-		} else {
-			Link newLink = new Link(data, null);
-			if (pos == 0) {
-				newLink.setNext(head);
-				head = newLink;
-			} else {
-				Link currLink = head;
-				int counter = 0;
-				while (counter < pos-1) {
-					currLink = currLink.getNext();
-				}
-				newLink.setNext(currLink.getNext());
-				currLink.setNext(newLink);
-			}
-			size++;
-		}
-	}
-		
-	public void remove(int pos) throws Exception {
-		/*1. Error check: Is position valid?
-		2. Special cases? First or last element
-		3. Loop to Link at pos-1
-		4. Store data to return
-		5. Manipulate pointers
-			link.next = link.next.next
-		6. Update size
-		7. Return data*/
-		
-		if (pos < 0 || pos > size) {
-			throw new Exception();
-		} else {
-			if (pos == 0) {
-				head = head.getNext();
-			} else {
-				Link currLink = head;
-				int counter = 0;
-				while (counter < pos-1) {
-					currLink = currLink.getNext();
-				}
-				currLink.setNext(currLink.getNext().getNext());
-				size--;
-			}
-		}
-	}
-		
-	public Object get(int pos) throws Exception {
-		if (pos < 0 || pos > size) {
-			throw new Exception();
-		} else {
-			if (pos == 0) {
-				return head.getData();
-			} else {
-				Link currLink = head;
-				int counter = 0;
-				while (counter < pos) {
-					currLink = currLink.getNext();
-				}
-				return currLink.getData();
-			}
-		}	
-	}
-	
-	public int size() {
-		return size;
-	}
-	
-	public ListIterator iterator() {
-		return new ListIterator();
 	}
 }
