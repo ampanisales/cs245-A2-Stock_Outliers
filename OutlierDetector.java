1459aff7e454122aae959ad385645209838a0dc4
ampanisales <31353791+ampanisales@users.noreply.github.com>
2017-10-20 15:04:01 -0700

    Update OutlierDetector.java

--- OutlierDetector.java
+++ OutlierDetector.java
@@ -1,9 +1,9 @@
 
 public class OutlierDetector {
 
-	/*independent variable (x) being the number of business days 
-	since the starting date (cf. Requirement 2) and the dependent 
-	variable (y) being the CLOSING price of the stock*/
+	/*independent variable (x) is the number of business days 
+	since the starting date and the dependent 
+	variable (y) is the CLOSING price of the stock*/
 	
 	private int sumX;
 	private double sumY;
@@ -29,7 +29,6 @@ public class OutlierDetector {
 		return (n*sumXY - sumX*sumY)/(n*sumXsquared - Math.pow(sumX, 2));
 	}
 
-	//EMAIL PROFESSOR ABOUT VARIANCE FORMULA
 	public double calculateVariance(double sumPriceMinusMean) { 
 		return sumPriceMinusMean/(n-1);
 	}
@@ -40,16 +39,11 @@ public class OutlierDetector {
 		int x = day;
 		double y = adjClose;
 		double stdev = Math.sqrt(variance);
-		double lowerLimit = mean - stdev;
-		double upperLimit = mean + stdev;
-		//System.out.println("a+b*x: " + (a+b*x) + " y: " + y + " variance: " + variance);
-		//ASK WHAT IS THE CRITERIA FOR AN OUTLIER
-		if (Math.abs((a+b*x) - y) > Math.abs((a+b*x) - variance)) {
+		double lowerLimit = (a+b*x) - stdev;
+		double upperLimit = (a+b*x) + stdev;
+		if (lowerLimit > y || upperLimit < y) {
 			return true;
-		}
-		/*if (lowerLimit > y || upperLimit < y) {
-			return true;
-		}*/ else {
+		} else {
 			return false;
 		}
 	}
