cb7d032163fe98f75b404e374e479365eb77e4ac
ampanisales <31353791+ampanisales@users.noreply.github.com>
2017-10-20 21:28:59 -0700

    Update OutlierDetector.java

--- OutlierDetector.java
+++ OutlierDetector.java
@@ -1,46 +1,26 @@
 
 public class OutlierDetector {
 
-	/*independent variable (x) is the number of business days 
-	since the starting date and the dependent 
-	variable (y) is the CLOSING price of the stock*/
+	int n; //Number of records in range
+	double mean;
+	double sumCloseMinusMean;
 	
-	private int sumX;
-	private double sumY;
-	private int n;
-	private double sumXY;
-	private int sumXsquared;
-	
-	
-	public OutlierDetector(int newSumX, double newSumY, int newN, double newSumXY,
-							int newSumXsquared) {
-		sumX = newSumX;
-		sumY = newSumY;
+	public OutlierDetector(int newN, double newSumCloseMinusMean, double newMean) {
 		n = newN;
-		sumXY = newSumXY;
-		sumXsquared = newSumXsquared;
-	}
-	
-	public double calculateA() {
-		return (sumY*sumXsquared - sumX*sumXY)/ (n*sumXsquared - Math.pow(sumX, 2));
-	}
-		
-	public double calculateB() {
-		return (n*sumXY - sumX*sumY)/(n*sumXsquared - Math.pow(sumX, 2));
+		sumCloseMinusMean = newSumCloseMinusMean;
+		mean = newMean;
 	}
 
-	public double calculateVariance(double sumPriceMinusMean) { 
-		return sumPriceMinusMean/(n-1);
+	public double calculateVariance() { 
+		return sumCloseMinusMean/(n-1);
 	}
 	
-	public boolean isOutlier(int day, double adjClose, double mean, double variance) {
-		double a = calculateA();
-		double b = calculateB();
-		int x = day;
+	public boolean isOutlier(double adjClose) {
 		double y = adjClose;
+		double variance = calculateVariance();
 		double stdev = Math.sqrt(variance);
-		double lowerLimit = (a+b*x) - stdev;
-		double upperLimit = (a+b*x) + stdev;
+		double lowerLimit = mean - 2*stdev;
+		double upperLimit = mean + 2*stdev;
 		if (lowerLimit > y || upperLimit < y) {
 			return true;
 		} else {
