285e21528cf0808ee59937b667daecda86831fa8
ampanisales <31353791+ampanisales@users.noreply.github.com>
2017-10-26 11:47:43 -0700

    Update OutlierDetector.java

--- OutlierDetector.java
+++ OutlierDetector.java
@@ -1,8 +1,48 @@
 /**
-* OutlierDetector assists StockOutliers in
-* finding stock records that are outliers,
-* and it uses the mean and standard
-* deviation method for finding outliers
+* Class Purpose: OutlierDetector assists 
+* StockOutliers in finding stock records 
+* that are outliers, and it uses the mean 
+* and standard deviation method for finding
+* outliers
+*
+* @author Anthony Panisales
+*/
+
+
+public class OutlierDetector {
+
+	int n; //Number of records in range
+	double mean;
+	double sumCloseMinusMean;
+	
+	public OutlierDetector(int newN, double newSumCloseMinusMean, double newMean) {
+		n = newN;
+		sumCloseMinusMean = newSumCloseMinusMean;
+		mean = newMean;
+	}
+
+	public double calculateVariance() { 
+		return sumCloseMinusMean/(n-1);
+	}
+	
+	public boolean isOutlier(double adjClose) {
+		double variance = calculateVariance();
+		double stdev = Math.sqrt(variance);
+		double lowerLimit = mean - 2*stdev;
+		double upperLimit = mean + 2*stdev;
+		if (lowerLimit > adjClose || upperLimit < adjClose) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+}/**
+* Class Purpose: OutlierDetector assists 
+* StockOutliers in finding stock records 
+* that are outliers, and it uses the mean 
+* and standard deviation method for finding
+* outliers
 *
 * @author Anthony Panisales
 */
